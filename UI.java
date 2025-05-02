eccc928272de00280c3db44b5739ced23726c6d7
MomenOsama123 <omomen519@gmail.com>
2025-05-02 18:36:15 +0300

    .....

---
+++ UI.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame; 
+    public class UI{
+    JFrame frame=new JFrame(); 
+    frame.setTitle("Flight Booking Management System Project");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(800,600);
+    frame.setResizable(false); 
+    frame.setvisable(true); 
+}
